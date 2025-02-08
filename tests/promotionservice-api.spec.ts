c9910d4c3589d24b7da5935636cee29371048823
rBotPlanet <rbotplanet@gmail.com>
2025-02-08 16:45:36 +0700

    Commit initial automation test

---
+++ tests/promotionservice-api.spec.ts
@@ -0,0 +1,40 @@
+import {test, expect} from '@playwright/test';
+
+test.describe('PromotionService API Tests', () => {
+    let userId: string;
+
+    test('POST - ConvertOverDiscountPayment', async ({ request }) => {
+        const response = await request.post('http://10.3.0.121/PromotionService/api/ConvertOverDiscountPayment', {
+          data: {
+            "PosConnectionString": "Data Source=10.3.8.223;Initial Catalog=KPI-POS-HANA;Persist Security Info=True;User ID=sa;Password=sql2000",
+            "EcouponDBConnectionString": "Data Source=10.3.8.223;Initial Catalog=eCoupon;Persist Security Info=True;User ID=sa;Password=sql2000",
+            "StrCode": "Z99",
+            "BranchNo": "40",
+            "SubBranch": "MHN-HANA",
+            "ProductItem": [
+                {
+                    "Line": 1,
+                    "Article": "000000000003016824",
+                    "Cate": "371",
+                    "Brand": "MLW",
+                    "Net": 300.0,
+                    "DiscPercent": null,
+                    "DiscAmt": 0.0,
+                    "IsMaxDisc": false,
+                    "MaxPercent": null,
+                    "DiscSum": 0.0,
+                    "IsLockDiscount": false,
+                    "Freeze": null,
+                    "IsCalculate": false
+                }
+            ],
+            "PromoList": [],
+            "GwlNo": "",
+            "ObjPrivilege": {
+                "CurrentPrivilege": null,
+                "PreviousPrivilege": null
+            }
+         }
+        });
+    });
+  });
